89c19529cda3ecb800cef98e0b63bb8edc051598
ridiculoys <lltalip@up.edu.ph>
2020-03-13 10:48:15 +0800

    Appwar was working before I finished working on the fragments. After fragments are fully functioning, appbar stopped working.

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -2,6 +2,7 @@ package com.example.android.navigation
 
 
 import android.os.Bundle
+import android.util.Log
 import android.view.*
 import androidx.fragment.app.Fragment
 import android.widget.TextView
@@ -18,7 +19,6 @@ class TitleFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val binding: FragmentTitleBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false)
-
         binding.playButtonTitle.setOnClickListener{view : View ->
             view.findNavController().navigate(R.id.action_titleFragment_to_nameFragment)}
 
@@ -28,11 +28,12 @@ class TitleFragment : Fragment() {
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
-        inflater.inflate(R.menu.options_menu, menu)
+        inflater.inflate(R.menu.options_menu_2, menu)
+        Log.d("hi: ", "created")
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return NavigationUI.onNavDestinationSelected(item,
+        return NavigationUI.onNavDestinationSelected(item!!,
                 view!!.findNavController())
                 || super.onOptionsItemSelected(item)
     }
